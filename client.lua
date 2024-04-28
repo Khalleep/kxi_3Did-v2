8087cbf9f6686859fe2bb39f582f48aac9dc5379
Khalexiii <144989632+Khalleep@users.noreply.github.com>
2024-04-28 14:31:09 +0300

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,57 @@
+local isIdDisplaying = false
+
+RegisterCommand("toggleid", function()
+    isIdDisplaying = not isIdDisplaying
+    if isIdDisplaying then
+            lib.notify({
+                title = 'Player IDs',
+                description = 'Player IDs are now on',
+                type = 'info'
+            })
+        else
+            lib.notify({
+                title = 'Player IDs',
+                description = 'Player IDs are now off',
+                type = 'info'
+            })
+    end
+end)
+
+RegisterKeyMapping("toggleid", "Toggle Player IDs", "keyboard", "pageup")
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+
+        if isIdDisplaying then
+            local playerPed = PlayerPedId()
+            local playerCoords = GetEntityCoords(playerPed)
+
+            DrawPlayerId(playerCoords.x, playerCoords.y, playerCoords.z + 1.2, GetPlayerServerId(PlayerId()))
+
+            for _, player in ipairs(GetActivePlayers()) do
+                local targetPed = GetPlayerPed(player)
+                local targetCoords = GetEntityCoords(targetPed)
+                local distance = #(playerCoords - targetCoords)
+
+                if distance < 20.0 and playerPed ~= targetPed then
+                    DrawPlayerId(targetCoords.x, targetCoords.y, targetCoords.z + 1.2, GetPlayerServerId(player))
+                end
+            end
+        end
+    end
+end)
+
+function DrawPlayerId(x, y, z, id)
+    local onScreen, _x, _y = World3dToScreen2d(x, y, z)
+    if onScreen then
+        SetTextScale(0.31, 0.31)
+        SetTextFont(0)
+        SetTextProportional(1)
+        SetTextColour(255, 255, 255, 215)
+        SetTextEntry('STRING')
+        SetTextCentre(1)
+        AddTextComponentString(tostring(id))
+        DrawText(_x, _y)
+    end
+end
---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Khalexi'
+description 'Show ID script made by KXI'
+lua54 'yes'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua'
+}
